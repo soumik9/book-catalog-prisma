bc7d60a1bb0a757d1851dd45e07a5bed878cf435
soumik9 <soumik.ahammed.9@gmail.com>
2023-09-02 13:22:31 +0600

    update a user, only admin

---
+++ app/controllers/user/UpdateUser.ts
@@ -0,0 +1,28 @@
+import { Request, Response, RequestHandler } from 'express';
+import catchAsync from '../../../utils/helpers/catchAsync';
+import httpStatus from 'http-status'
+import sendResponse from '../../../utils/helpers/SendResponse';
+import prisma from '../../../utils/helpers/prisma';
+
+
+const UpdateUser: RequestHandler = catchAsync(
+    async (req: Request, res: Response) => {
+
+        // update user
+        const result = await prisma.user.update({
+            where: {
+                id: req.params.id
+            },
+            data: req.body
+        });
+
+        sendResponse(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: 'User updated successfully!',
+            data: result,
+        });
+    }
+)
+
+export default UpdateUser;
--- app/routers/userRouter.ts
+++ app/routers/userRouter.ts
@@ -3,10 +3,12 @@ import GetUsers from '../controllers/user/GetUsers';
 import auth from '../middlewares/auth';
 import { ENUM_USER_ROLE } from '../../utils/constatnts';
 import GetUser from '../controllers/user/GetUser';
+import UpdateUser from '../controllers/user/UpdateUser';
 const router = express.Router();
 
 //routes
 router.get('/:id', auth(ENUM_USER_ROLE.ADMIN), GetUser);
 router.get('/', auth(ENUM_USER_ROLE.ADMIN), GetUsers);
+router.patch('/:id', auth(ENUM_USER_ROLE.ADMIN), UpdateUser);
 
 export default router;
